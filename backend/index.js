b5f219418b1635e0d73a4064d3cfd0bab2c10689
cyzhung <cyzhung@gmail.com>
2025-01-14 17:08:41 +0800

    update buy api

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
-const usersRoutes = require('./routes/usersApi');  // 引入路由
-const stocksRoutes = require('./routes/stocksApi');
+const usersRoutes = require('./routes/usersAPI');  // 引入路由
+const stocksRoutes = require('./routes/stocksAPI');
+const userSrocksRoutes = require('./routes/userStocksAPI');
+
 
 app.use(express.json());
 // 使用 API 路由
 app.use('/api/users', usersRoutes);  // 這裡設置了路徑前綴 /api
 app.use('/api/stocks', stocksRoutes);
-
+app.use('/api/userStocks', userSrocksRoutes)
 // 設置根路徑的處理器（可選）
 app.get('/', (req, res) => {
   res.send('Welcome to the Stock Tracker API!');
